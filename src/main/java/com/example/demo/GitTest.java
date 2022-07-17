baab945af914b341fda32769ebae1c1122a25795
YinMingWei <JuRan@qq.com>
2022-07-17 21:31:58 +0800

    two 1 commit

--- src/main/java/com/example/demo/GitTest.java
+++ src/main/java/com/example/demo/GitTest.java
@@ -3,6 +3,8 @@ package com.example.demo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello github");
+        System.out.println("hello git and github");
         System.out.println("hello miss");
     }
 
